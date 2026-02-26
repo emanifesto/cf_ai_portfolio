dbd0ad452818f985161d821ece7a4215b2f479e8
emanifesto <emmanuelbabayemi37@gmail.com>
2026-02-26 15:59:27 -0500

    navigation template added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import OuterContainer from './components/container'
+import OuterContainer from './components/container.tsx'
+import Navigation from './components/nav.tsx'
 
 function App() {
 
   return (
     <main className='h-screen overflow-scroll no-scrollbar bg-radial from-dark-blue/85 to-dark-blue'>
+      <Navigation />
       <OuterContainer>
         <p>HI</p>
       </OuterContainer>
---
+++ src/components/nav.tsx
@@ -0,0 +1,22 @@
+export default function Navigation(){
+    const selected = "Home"
+    return(
+        <div className="flex gap-7 rounded-full bg-black/20 backdrop-blur-3xl drop-shadow-xs drop-shadow-whie px-5 pb-3 pt-2 fixed top-4 left-[50%] translate-x-[-50%] border-black border-t-2 border-l-2">
+            <Section title="Home" selected={selected}/>
+            <Section title="Education" selected={selected}/>
+            <Section title="Experience" selected={selected}/>
+            <Section title="Projects" selected={selected}/>
+            <Section title="Extracurricular" selected={selected}/>
+            <Section title="Skills" selected={selected}/>
+        </div>
+    )
+}
+
+function Section({ title, selected }: {title: string, selected: string}){
+    const textColor = selected == title ? 'text-gray-200' : 'text-gray-500'
+    return (
+        <a className={`text-2xl ${textColor} hover:text-gray-200`}>
+            <p>{title}</p>
+        </a>
+    )
+}
